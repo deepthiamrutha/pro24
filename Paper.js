cff345211648cc39d07db2041f59a1e7509306f2
deepthiamrutha <68592979+deepthiamrutha@users.noreply.github.com>
2020-08-04 16:35:06 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,25 @@
+class Paper{
+constructor(x,y) {
+    var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    this.body = Bodies.circle(x, y,70,options);
+    this.height = 50;
+    this.width = 50;
+
+    World.add(world, this.body);
+}
+display() {
+var pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      stroke("black");
+      fill("lightBlue");
+      ellipseMode(CENTER);
+      ellipse(0,0,this.width,this.height);
+      pop();
+}
+}
